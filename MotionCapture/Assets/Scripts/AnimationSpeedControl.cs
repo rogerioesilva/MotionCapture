148296afa9b94526c100d2eecfe48e29bbeca799
rogerioesilva <professor.rogerio@gmail.com>
2019-12-26 22:57:04 -0600

    Josiah first mocap session

---
+++ MotionCapture/Assets/Scripts/AnimationSpeedControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeedControl : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            _animator.SetTrigger("Regular");
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            _animator.SetTrigger("Fast");
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            _animator.SetTrigger("Slow");
+    }
+}
--- MotionCapture/Assets/Scripts/Rotate.cs
+++ MotionCapture/Assets/Scripts/Rotate.cs
@@ -6,10 +6,22 @@ public class Rotate : MonoBehaviour
 {
     public float RotationalSpeed = 5f;
     public Transform referencePoint;
+    public float FOV = 60.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(referencePoint.position, Vector3.up, RotationalSpeed * Time.deltaTime);
+        float axis = 0f;
+        if (Input.GetKey("left"))
+            axis = -1f;
+        if (Input.GetKey("right"))
+            axis = 1f;
+        if (Input.GetKey("up"))
+            FOV -= 1f;
+        if (Input.GetKey("down"))
+            FOV += 1f;
+
+        transform.RotateAround(referencePoint.position, Vector3.up, -axis * RotationalSpeed * Time.deltaTime);
+        Camera.main.fieldOfView = FOV;
     }
 }
